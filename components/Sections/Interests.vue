518ab094e0998b937fa869c246173015a49e73cc
DeveloperThomas <tomunderwood.dev@gmail.com>
2020-12-22 22:25:07 +1000

    Adding everything.

---
+++ components/Sections/Interests.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my-auto">
+    <h2 class="mb-5">{{ $t('interests.title') }}</h2>
+    <p>
+      {{ $t('interests.firstBlock') }}
+    </p>
+    <p class="mb-0">
+      {{ $t('interests.secondBlock') }}
+    </p>
+    <img src="/Thomas-Underwood-soccer.jpg" class="about-photo"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { };
+  }
+};
+</script>
