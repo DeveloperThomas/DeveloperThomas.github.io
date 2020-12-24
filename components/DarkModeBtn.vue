0f8c1123303a51d81edc4afb7d40b1ef0edaf485
DeveloperThomas <tomunderwood.dev@gmail.com>
2020-12-24 12:26:47 +1000

    Adding settieout and async await for color fix.

--- components/DarkModeBtn.vue
+++ components/DarkModeBtn.vue
@@ -11,7 +11,11 @@ export default {
   mounted(){
     this.setLightMode();
 
-    setTimeout(() => {this.colorMode = light}, 3000)
+    setTimeout(() => {this.setLightMode()}, 3000)
+
+    async function x() {
+      await setTimeout(() => {this.setLightMode()}, 3000)
+    }
   },
   data() {
     return {
